1f8c94ee3eef4871b8d2bbebda1bd810c1e1dd23
backsft <kabirmia526@gmail.com>
2024-11-26 01:42:10 +0600

    class01

--- src/App.js
+++ src/App.js
@@ -2,22 +2,42 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let name = "Mr Jhon";
+  let x = 10;
+  let y = 27;
+  let obj={
+    "name" : "ahmed",
+    "age" : 19
+  }
+
+  let status=true;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <h1>{name}</h1>
+      <h2>{x*y}</h2>
+      <h2>{obj.name}</h2>
+      <h2>{obj.age}</h2>
+      {
+        x > y ? (
+          <div>x is greater than y</div>
+        ) : (
+          <div>x is not greater than y</div>
+        )
+      }
+
+  {
+        obj.age > 18 ? (
+          <div>{obj.name} your age is {obj.age}</div>
+        ) : (
+          ""
+        )
+      }
+      
+      {
+        (status) ? <h1 style={{color:"red",backgroundColor:'yellow'}}>Welcome to Project</h1>: "" 
+      }
+     
     </div>
   );
 }
